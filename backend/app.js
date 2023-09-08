6605e8cd3c3abf54618a495c7f504b8c243d3956
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-09-08 17:43:27 +0300

    fix: fix .env

--- backend/app.js
+++ backend/app.js
@@ -5,31 +5,48 @@ const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const NotFoundError = require('./components/NotFoundError');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 dotenv.config();
-const {
-  PORT = 3000,
-  NODE_ENV,
-  ORIGIN,
-  DB_CONN,
-} = process.env;
+const { PORT = 3000, NODE_ENV, ORIGIN, DB_CONN } = process.env;
 const app = express();
 
 /**
  * безопасность приложения (количество запросов и заголовки)
  */
-app.use(
-  cors({
-    origin: NODE_ENV === 'production' ? ORIGIN : 'http://localhost:3001',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    credentials: true,
-  }),
-);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://likee.nomoredomainsicu.ru');
+
+  if (req.method === 'OPTIONS') {
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    );
+  }
+
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type',
+  );
+
+  res.header('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
+// app.use(
+//   cors({
+//     origin: NODE_ENV === 'production' ? ORIGIN : 'http://localhost:3001',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     allowedHeaders:
+//       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+//     credentials: true,
+//   })
+// );
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -55,7 +72,7 @@ mongoose.set('toJSON', { useProjection: true });
 /**
  * подключение базы данных
  */
-mongoose.connect(DB_CONN, {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   autoIndex: true,
